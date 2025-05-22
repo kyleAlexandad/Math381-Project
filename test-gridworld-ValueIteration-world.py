9e603561bb2917f4db031d7b34a0d8f131ffc1f7
Justin Chae <jcjustin.chae@gmail.com>
2025-05-22 02:45:43 -0700

    updated sunset gridworld

--- test-gridworld-ValueIteration-world.py
+++ test-gridworld-ValueIteration-world.py
@@ -3,11 +3,11 @@ import numpy as np
 from GridWorld import GridWorld
 from ValueIteration import ValueIteration
 
-problem = GridWorld('data/world.csv', reward={0: -0.04, 1: 5.0, 2: -5.0, 3: np.NaN}, random_rate=0.2)
+problem = GridWorld('data/sunset.csv', reward={0: -0.04, 1: 5.0, 2: -5.0, 3: np.NaN}, random_rate=0.2)
 
 
 solver = ValueIteration(problem.reward_function, problem.transition_model, gamma=0.9)
 solver.train()
 
 problem.visualize_value_policy(policy=solver.policy, values=solver.values)
-problem.random_start_policy(policy=solver.policy, start_pos=(5, 3), n=1000)
+problem.random_start_policy(policy=solver.policy, start_pos=(4, 17), n=1000)
